686e85cb04d5f4aacda9cdf212123b3e67043b9d
Akhil654N <akhilnethi.jhcs@gmail.com>
2025-02-06 10:53:49 +0530

    Add files via upload

---
+++ Demo062.java
@@ -0,0 +1,9 @@
+public class Demo062 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println("First element: " + arr[0]);
+        System.out.println("Second element: " + arr[1]);
+        System.out.println("Last element: " + arr[arr.length-1]);
+    }
+
+}
---
+++ Demo063.java
@@ -0,0 +1,8 @@
+public class Demo063 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println("First element: " + arr[0]);
+        System.out.println("Last element: " + arr[arr.length-1]);
+        System.out.println("Sum of first and last element: " + (arr[0] + arr[arr.length-1]));
+    }
+}
